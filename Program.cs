da3fe563930c25472d1675cf3d8df3e578037721
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2022-11-02 12:11:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace BinarySearchCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Program program = new Program();
+            int result = program.Search(new int[] { 5 }, -5);
+        }
+
+        public int Search(int[] nums, int target)
+        {
+            bool found = false;
+            int index = -1;
+            Parallel.For(0, nums.Length, (i) =>
+            {
+                if (found)
+                    return;
+                if (nums[i] == target)
+                {
+                    index = i;
+                    return;
+                }
+            });
+            return index;
+        }
+    }
+}
+
+/*
+ This problem can be solved with mutliple way.
+
+1. Write a for loop
+2. Write a do While loop.
+3. Write a parallel loop
+4. Multi pointer loop
+ */
